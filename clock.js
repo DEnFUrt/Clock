6047b138e4ba78ed56b7221219d3ef08af2b3e7c
Denis <57797457+DEnFUrt@users.noreply.github.com>
2020-03-16 22:11:54 +0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,69 @@
+/* 
+Модуль таймера обратного отсчета 
+Для выставления таймера в UTC+0 необходимо передавать 
+параметр endTime при вызове класса Сlock в коротком формате 'YYYY-MM-DD'
+Для учета часового пояса пользователя параметр endTime
+необходимо передавать в формате YYYY-MM-DDTHH:mm:ss.sss
+Для корректировки часового пояса используйте полный формат даты
+YYYY-MM-DDTHH:mm:ss.sssZ, где Z часовой пояс в формате +-hh:mm от UTC+0  
+*/
+
+export class Clock {
+    constructor (timerId, endTime) {
+        this.endTime = endTime;
+        this.timer = document.querySelector(`#${timerId}`);
+        this.hours = this.timer.querySelector('.hours');
+        this.minutes = this.timer.querySelector('.minutes');
+        this.seconds = this.timer.querySelector('.seconds');
+    }
+
+    _getTime () {
+        const deltaTime = Date.parse(this.endTime) - Date.now(),
+            seconds = Math.floor( (deltaTime / 1000) % 60 ),
+            minutes = Math.floor( (deltaTime / 1000 / 60) % 60),
+            hours = Math.floor( (deltaTime / (1000 * 60 * 60)) );
+    
+        return {
+            deltaTime,
+            hours,
+            minutes,
+            seconds,
+        }
+    }
+
+    _addLeadZero (tempTime) {
+        let clockLED;
+        
+        switch(true) {
+            case tempTime < 0 :
+                clockLED = '00';
+                break;
+            case tempTime <= 9 :
+                clockLED = `0${tempTime.toString()}`;
+                break;
+            default :
+                clockLED = tempTime.toString();
+                break; 
+        }
+    
+        return clockLED;
+    }
+
+    _render(interimClock) {
+        this.hours.textContent = this._addLeadZero(interimClock.hours);
+        this.minutes.textContent = this._addLeadZero(interimClock.minutes);
+        this.seconds.textContent = this._addLeadZero(interimClock.seconds);
+    }
+
+    start() {
+        const id = setInterval( () => {
+            let interimClock = this._getTime();
+
+            if (interimClock.deltaTime <= 0) {
+                clearInterval(id);
+                console.log("Clock -> id -> clearInterval(id)", id)
+            }
+            this._render(interimClock);
+         });
+    }
+}
